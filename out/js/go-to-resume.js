a02d0106a30ec3e2be7a3859cf40c9e6b51fd407
Sequoia McDowell <sequoia@eventmobi.com>
2019-08-19 18:08:18 -0400

    adding resume easter egg

---
+++ out/js/go-to-resume.js
@@ -0,0 +1,23 @@
+target = 'resume'.split('');
+nextKey = 0;
+
+if((new KeyboardEvent('')).key !== 'undefined'){
+    document.addEventListener('DOMContentLoaded', typeToResume)
+}
+
+/**
+ * send the user to my resume if they type "resume"
+ */
+function typeToResume(){
+    document.body.addEventListener('keydown', function(e){
+        if(e.key.toLowerCase() === target[nextKey]){
+            nextKey++;
+            if(nextKey === target.length){
+            //they typed "resume"! Send user to resume
+            window.location = '/s/resume.pdf'
+            }
+        }else{
+            nextKey = 0;
+        }
+    });   
+}
